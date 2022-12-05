ec0f5030ae210657a6fc19525bed3d6d7f2aee54
subhrendu1987 <subhrendu@idrbt.ac.in>
2022-12-05 13:53:48 +0530

    modified:   DuplexPrint.py

--- DuplexPrint.py
+++ DuplexPrint.py
@@ -2,11 +2,17 @@
 import argparse
 from PyPDF2 import PdfReader
 import os
+import subprocess
 #############################################################
 parser = argparse.ArgumentParser(description='Print PDF pages in Duplex mode using a single function printer')
-parser.add_argument('filename')           # positional argument
-
+parser.add_argument('filename')# positional argument
 args = parser.parse_args()
+#############################################################
+def evenRangeRev(end,start=1):
+	end= end if end % 2 == 0 else end-1
+	# iterating each number in list
+	rng=range(end,start,-2)
+	return(rng)
 #############################################################
 def getPDFPages(filename):
 	reader = PdfReader(filename)
@@ -15,17 +21,27 @@ def getPDFPages(filename):
 #############################################################
 def printFile(filename):
 	pageCount= getPDFPages(filename)
-	print(pageCount)
-	CMD="lpr -o sides=two-sided-short-edge "+filename
-	print(CMD)
-	print("Printing ODD pages")
-	CMD="lpr -o page-set=odd "+filename
-	#os.system(CMD)
-	print(CMD)
-	inp = input('Please wait till the printer stops and then enter to continue ...')
-	print("Printing EVEN pages")
-	CMD="lpr -o page-set=even -o outputorder=reverse "+filename
-	#os.system(CMD)
-	print(CMD)
+	print("Total number of pages = "+str(pageCount))
+	##CMD="lpr -o sides=two-sided-short-edge "+filename
+	##print(CMD)
+	############################################
+	oddPages=range(1, pageCount, 2) 
+	for i in oddPages:
+		print("Printing page "+str(i)+"/"+str(pageCount))
+		CMD="lpr -o page-ranges="+str(i)+" "+filename
+		#os.system(CMD)
+		#p = subprocess.Popen(CMD.split())
+		#p.wait()
+		#print(CMD)
+	############################################
+	inp = input('Please wait till the printer stops\n and then place the pages in tray.\n Now enter to continue ...')
+	############################################
+	evenPages=evenRangeRev(pageCount) 
+	for i in evenPages:
+		print("Printing page "+str(i)+"/"+str(pageCount))
+		CMD="lpr -o page-ranges="+str(i)+" "+filename
+		p = subprocess.Popen(CMD.split())
+		p.wait()
+		#print(CMD)
 #############################################################
 printFile(args.filename)
